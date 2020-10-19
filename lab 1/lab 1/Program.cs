fbd3fb11946a509e5459ba658675157e3cebac45
Adittya <arup2140@gmail.com>
2020-10-19 11:03:28 +0600

    lab tasks

---
+++ lab 1/lab 1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace lab_1
+{
+    class Program
+    {
+        /*(1) static void Main(string[] args)
+         {
+             Console.WriteLine("Hello");
+             Console.WriteLine("Adittya");
+         }*/
+
+        /*(2) static void Main(string[] args)
+         {
+             int a = 5;
+             int b = 10;
+             int sum = a + b;
+             Console.WriteLine("The sum of a = 5 & b = 10 is : " + sum);
+
+
+         }*/
+
+        /* (3) static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 3;
+            int division = a / b;
+            Console.WriteLine("The division of " + a + " and "+ b+ " is : " + division);
+        
+        }*/
+
+        /*(4) static void Main(string[] args)
+        {
+            double a = -5 + 8 * 6;
+            Console.WriteLine(a);
+            double b = (55 + 9) % 9;
+            Console.WriteLine(b);
+            double c = 20 + -3 * 8 / 5;
+            Console.WriteLine(c);
+            double d = 5 + 15 / 3 * 2 / 8 % 3;
+            Console.WriteLine(d);
+        } */
+
+
+        /*(5)   static void Main(string[] args)
+           {
+               int a = 6, b = 3;
+               Console.WriteLine("The addition of " + a + " and " + b + " is = " + (a + b));
+               Console.WriteLine("The subtraction of " + a + " and " + b + " is = " + (a - b));
+               Console.WriteLine("The multiplication of " + a + " and " + b + " is = " + (a * b));
+               Console.WriteLine("The division of " + a + " and " + b + " is = " + (a / b));
+               Console.WriteLine("The remainder of " + a + " and " + b + " is = " + (a % b));
+           } */
+
+        /*(6) static void Main(string[] args)
+         {
+             float a = 25.5f;
+             float b = 3.5f;
+             float c = 40.5f;
+             float d = 4.5f;
+             float sum = ((a * b - b * b) / (c - d));
+
+             Console.WriteLine("The result of ((25.5*3.5-3.5*3.5)/(40.5-4.5)) is= " + sum);
+
+         } */
+
+
+        /*(7) static void Main(string[] args) 
+         {
+             double width = 5.6;
+             double height = 8.5;
+             double area, perimeter;
+             area = width * height;
+             perimeter = 2 * (width + height);
+
+             Console.WriteLine("Area is 5.6*8.5 = " + area);
+             Console.WriteLine("Perimeter is 2*(5.6+8.5) = " + perimeter);
+         }*/
+
+
+
+
+
+    }
+}
---
+++ lab 2/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace lab_2
+{
+    class Program
+    {
+        /*(1)  static void Main(string[] args)
+          {
+              int n = 100;
+              Console.WriteLine("Even Numbers from 1 to " + n + " are: ");
+              for (int i = 1; i <= n; i++)
+              {
+
+                  if (i % 2 == 0)
+                  {
+                      Console.WriteLine(i + " ");
+
+                  }
+
+              }
+
+          }*/
+
+
+
+
+        /*(2)  static void Main(string[] args)
+          {
+              for (int i = 0; i < 4; i++)
+              {
+                  for (int j = 0; j < 6; j++)
+                  {
+                      if (i % 2 == 0)
+                      {
+                          if (j % 2 == 0)
+                              Console.Write("X");
+                          else
+                              Console.Write("0");
+                      }
+
+                      else
+                      {
+                          if (j % 2 == 0)
+                              Console.Write("0");
+                          else
+                              Console.Write("X");
+                      }
+
+                  }
+                  Console.WriteLine();
+
+
+              } */
+
+        /*(3)  static void Main(string[] args)
+          {
+              for (int i = 1; i <= 5; i++)
+              {
+                  int j = i;
+                  int count = 0;
+                  while (count < 5)
+                  {
+                      Console.Write(j);
+                      count++;
+                      j++;
+                  }
+                  Console.WriteLine();
+
+          }*/
+
+
+
+
+
+        /*(4) static void Main(string[] args)
+         {
+             int i, num=100, odd_sum = 0, even_sum = 0;
+
+             for (i = 1; i <= num; i++)
+             {
+                 if (i % 2 == 0)
+                     even_sum = even_sum + i;
+                 else
+                     odd_sum = odd_sum + i;
+             }
+             Console.WriteLine("Sum of all odd numbers  = "+ odd_sum);
+             Console.WriteLine("Sum of all even numbers = "+ even_sum);
+
+         } */
+
+
+
+
+       /*(5) static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(" ");
+
+                }
+                Console.WriteLine(""+i);
+
+            }
+
+
+        } */
+
+
+        /* (6) static void Main(string[] args)
+         {
+
+             int i, num, fact = 1;
+             Console.WriteLine(" Please input the number:");
+             num = Convert.ToInt32(Console.ReadLine());
+             for (i = 1; i <= num; i++)
+             {
+                 fact = fact * i;
+             }
+
+             Console.WriteLine(" The factorial of " + num + " is= " + fact);
+
+         } */
+
+        /* (7)  static void Main(string[] args)
+          {
+              for (int i = 1; i <= 5; i++)
+              {
+                  for (int j = 1; j <= i; j++)
+                  {
+                      Console.Write("*");
+
+                  }
+                  Console.WriteLine("");
+
+              }
+
+
+              for (int k = 4; k >= 0; --k)
+              {
+
+                  for (int j = 1; j <= k; j++)
+                  {
+                      Console.Write("*");
+
+                  }
+                  Console.WriteLine("");
+              }
+
+          } */
+
+    }
+}
