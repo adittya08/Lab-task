2c9a2deeed1eaf5bea00513dc621940a89c146a5
Adittya <arup2140@gmail.com>
2020-11-25 09:52:51 +0600

    first commit within 10 am

---
+++ Lab 7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICalculator mycalc;
+            mycalc = new MyCalc();
+            mycalc.Sum(5, 5);
+
+            MyCalc mc = new MyCalc();
+            mc.Sum(5, 5);
+        }
+    }
+
+    public interface ICalculator
+    {
+        void Sum(int a, int b);
+    }
+
+    public class MyCalc : ICalculator
+    {
+        public void Sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+}
